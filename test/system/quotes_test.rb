55f42d8ed1088260803b7225523aac5b1533b52f
Divyesh Patel <pateldivyesh009@gmail.com>
2022-03-19 22:09:12 +0530

    turbo frames added for edit, update, delete, etc

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -6,25 +6,17 @@ class QuotesTest < ApplicationSystemTestCase
   end
 
   test "Creating a new quote" do
-    # When we visit the Quotes#index page
-    # we expect to see a title with the text "Quotes"
     visit quotes_path
-    assert_selector "h1", text: "Quotes"
+    assert_selector 'h1', text: 'Quotes'
 
-    # When we click on the link with the text "New quote"
-    # we expect to land on a page with the title "New quote"
-    click_on "New quote"
-    assert_selector "h1", text: "New quote"
+    click_on 'New quote'
+    fill_in 'Name', with: 'Capybara quote'
 
-    # When we fill in the name input with "Capybara quote"
-    # and we click on "Create Quote"
-    fill_in "Name", with: "Capybara quote"
-    click_on "Create quote"
+    assert_selector 'h1', text: 'Quotes'
+    click_on 'Create quote'
 
-    # We expect to be back on the page with the title "Quotes"
-    # and to see our "Capybara quote" added to the list
-    assert_selector "h1", text: "Quotes"
-    assert_text "Capybara quote"
+    assert_selector 'h1', text: 'Quotes'
+    assert_text 'Capybara qoute'
   end
 
   test 'Showing a quote' do
@@ -36,16 +28,16 @@ class QuotesTest < ApplicationSystemTestCase
 
   test 'Updating the quote' do
     visit quotes_path
-    assert_selector 'h1', text: 'Quotes'
+    assert_selector "h1", text: "Quotes"
 
-    click_on 'Edit', match: :first
-    assert_selector 'h1', text: 'Edit Quote'
+    click_on "Edit", match: :first
+    fill_in "Name", with: "Updated quote"
 
-    fill_in 'Name', with: 'Updated quote'
-    click_on 'Update quote'
+    assert_selector "h1", text: "Quotes"
+    click_on "Update quote"
 
-    assert_selector 'h1', text: 'Quotes'
-    assert_text 'Updated quote'
+    assert_selector "h1", text: "Quotes"
+    assert_text "Updated quote"
   end
 
   test 'Delete the quote' do
